7eae3fd0781f29bc4325ec50ceb2a5e0e8a97fa3
Jasper <twicejr@gmail.com>
2014-05-29 10:10:56 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+    require('sys' . DIRECTORY_SEPARATOR . 'boot.php');
+
+    //Determine the environment. This is not used somewhere else yet.
+    if($_SERVER['SERVER_NAME'] == 'localhost' || strstr($_SERVER['SERVER_NAME'], '192.168.'))
+    {
+        $env = App::ENV_DEVELOPMENT;
+    }
+    elseif(stristr($_SERVER['SERVER_NAME'], 'visietest.nl'))
+    {
+        $env = App::ENV_STAGING;
+    }
+    else
+    {
+        $env = App::ENV_PRODUCTION;
+    }
+    
+    if($env !== App::ENV_PRODUCTION && isset($_GET['env']))
+    {
+        $env = $_GET['env'];
+    }
+    
+    //define('BLACKLIST_FOLDERS', 'mod/some,mod/another');
+    define('LEAN_ENVIRONMENT', $env);
+    define('LEAN_REWRITE', true);
+    
+    try //Execute the request
+    {
+        require('sys' . DS . 'setup.php');
+
+        //Execute the request
+        $output = Html::minify(Request::instance()->prepare()->execute()) 
+        .'<!-- ' . Debug::instance()->memoryPeak() . 'MB / ' 
+        . Debug::instance()->parsetime() . 'ms / ' 
+        . Debug::$query_counter . ' queries -->';
+              
+        $debug = '';
+        if(Debug::enabled() && Config::get('site.debug_regular'))
+        {
+            $debug = Html::minify(Debug::in()->render());
+        }
+        echo $output . $debug;
+    }
+    catch(Exception $e)
+    {
+        if(Debug::enabled())
+        {
+            echo Debug::exception($e);
+        }
+        else
+        {
+            exit;//todo: 404 / log..
+        }
+    }
